7697fbbb1d343f57df2b252414fc347c64d9e1a7
Rob McDonald <rob.a.mcdonald@gmail.com>
2024-09-04 14:11:08 -0700

    Modify drawText wrapper to specify aspect ratio

--- src/vsp_graphic/include/TextMgr.h
+++ src/vsp_graphic/include/TextMgr.h
@@ -42,7 +42,7 @@ public:
     * x0, y0 - center location of the text.
     * xoffset, yoffset - offset from the center.
     */
-    static void drawText( glfont::GLFont * font, float scale, const std::string &str, float x0, float y0, float xoffset, float yoffset );
+    static void drawText( glfont::GLFont * font, float scale, const std::string &str, float x0, float y0, float xoffset, float yoffset, float arW = 1.f, float arH = 1.f );
 
 private:
 
