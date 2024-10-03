08702ed8607c347adbd09061c0f8601d19b4252b
Rob McDonald <rob.a.mcdonald@gmail.com>
2024-10-03 14:33:11 -0700

    Rename variable preset classes to be old_

--- src/geom_core/Vehicle.cpp
+++ src/geom_core/Vehicle.cpp
@@ -607,7 +607,7 @@ void Vehicle::Wype()
     DesignVarMgr.Renew();
     FitModelMgr.Renew();
     AnalysisMgr.Renew();
-    VarPresetMgr.Renew();
+    OldVarPresetMgr.Renew();
     ParasiteDragMgr.Renew();
     VSPAEROMgr.Renew();
     MeasureMgr.Renew();
@@ -1911,7 +1911,7 @@ xmlNodePtr Vehicle::EncodeXml( xmlNodePtr & node, int set )
     LinkMgr.EncodeXml( node );
     AdvLinkMgr.EncodeXml( node );
     VSPAEROMgr.EncodeXml( node );
-    VarPresetMgr.EncodeXml( node );
+    OldVarPresetMgr.EncodeXml( node );
     m_CfdSettings.EncodeXml( node );
     m_ISectSettings.EncodeXml( node );
     m_CfdGridDensity.EncodeXml( node );
@@ -2036,7 +2036,7 @@ xmlNodePtr Vehicle::DecodeXmlGeomsOnly( xmlNodePtr & node )
 
     LinkMgr.DecodeXml( node );
     AdvLinkMgr.DecodeXml( node );
-    VarPresetMgr.DecodeXml( node );
+    OldVarPresetMgr.DecodeXml( node );
     StructureMgr.DecodeXml( node );
     Background3DMgr.EncodeXml( node );
 
