325cf216c85f161079481ed8dddfa154fd6fdc0d
Timothy Cuatt <tcuatt@M4Engineering.local>
2024-10-14 14:58:21 -0700

    Revisions to TreeColumnWidget 	Clean up TreeColumnWidget codes 		Format consistent with rest of VSP 		Add attribution at top of page to Erco's cheatsheet 		Clarify comments 		Remove a redundant conditional and a redundant var assignment 		add more member var name convention where needed 		Move member vars to protected 		Add spaces to better communicate intent 		Add Erco's comment on code intent 	TreeColumnWidget changes cursor type 		Hand cursor type over browser should help clue in users to double click 	TreeColumnWidget add GetSelectedItems method 		pass a vec of treerowitems by reference, and it will find all currently-selected Fl_Tree_Items and convert them to TreeRowItems

--- src/gui_and_draw/TreeColumnWidget.cpp
+++ src/gui_and_draw/TreeColumnWidget.cpp
@@ -1,192 +1,302 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+// TreeColumnWidget.h: a modified Fl_Tree widget based on the code provided at Erco's 
+//             FLTK Cheat Page: http://seriss.com/people/erco/fltk/#Fl_Resize_Browser
+// Tim Cuatt
+//
+//////////////////////////////////////////////////////////////////////
+
 #include "TreeColumnWidget.h"
 
-TreeRowItem::TreeRowItem(Fl_Tree *tree, const char *text) : Fl_Tree_Item(tree) {
-this->label(text);
-m_LeftMargin = 5;
+TreeRowItem::TreeRowItem( Fl_Tree *tree, const char *text ) : Fl_Tree_Item( tree )
+{
+    this->label( text );
+    m_LeftMargin = 5;
 }
 
-int TreeRowItem::draw_item_content(int render) {
-  TreeWithColumns *treewc = (TreeWithColumns*)tree();
-  Fl_Color fg = drawfgcolor();
-  Fl_Color bg = drawbgcolor();
-  const int *col_widths = treewc->column_widths();
-  //    Show the date and time as two small strings
-  //    one on top of the other in a single item.
-  //
-  // Our item's label dimensions
-  int X = label_x(), Y = label_y(),
-      W = label_w(), H = label_h(),
-      RX = treewc->x() - treewc->hposition() + treewc->first_column_minw(),  // start first column at a fixed position
-      RY = Y+H-fl_descent();  // text draws here
-  // Render background
-  if ( render ) {
-    if ( is_selected() ) { fl_draw_box(prefs().selectbox(),X,Y,W,H,bg); }
-    else                 { fl_color(bg); fl_rectf(X,Y,W,H); }
-    fl_font(labelfont(), labelsize());
-  }
-  // Render columns
-  if ( render ) fl_push_clip(X,Y,W,H);
-  {
-    // Draw each column
-    //   ..or if not rendering, at least calculate width of row so we can return it.
-    //
-    int t=0;
-    const char *s = label();
-    char delim_str[2] = { treewc->column_char(), 0 };  // strcspn() wants a char[]
-    while ( *s ) {
-      int n = strcspn(s, delim_str);         // find index to next delimiter char in 's' (or eos if none)
-      if ( n>0 && render ) {                 // renderable string with at least 1 or more chars?
-        int XX = ( t==0 ) ? X : RX;          // TBD: Rename XX to something more meaningful
-        // Don't clip last column.
-        //    See if there's more columns after this one; if so, clip the column.
-        //    If not, let column run to edge of widget
-        //
-        int CW = col_widths[t];              // clip width based on column width
-        // If first column, clip to 2nd column's left edge
-        if ( t==0 ) { CW = (RX+col_widths[0])-XX; }
-        // If last column, clip to right edge of widget
-        if ( *(s+n) == 0 ) { CW = (x()+w()-XX); }
-        // Draw the text
-        //    We want first field (PID) indented, rest of fields fixed column.
-        //
-        fl_color(fg);
-        fl_push_clip(XX, Y, CW, H);          // prevent text from running into next column
-        fl_draw(s, n, XX+m_LeftMargin, RY);
-        fl_pop_clip();                       // clip off
-        // Draw vertical lines for all columns except first
-        if ( t>0 && treewc->column_separators() ) {
-          fl_color(FL_BLACK);
-          fl_line(RX,Y,RX,Y+H);
+int TreeRowItem::draw_item_content( int render_flag )
+{
+    TreeWithColumns *treewc = ( TreeWithColumns* )tree();
+    Fl_Color fg = drawfgcolor();
+    Fl_Color bg = drawbgcolor();
+    const int *m_ColWidths = treewc->column_widths();
+
+    // Our item's label dimensions
+    int X = label_x(), Y = label_y(),
+        W = label_w(), H = label_h(),
+        RX = treewc->x() - treewc->hposition() + treewc->first_column_minw(),
+        RY = Y+H-fl_descent();
+
+    // Render background
+    if ( render_flag )
+    {
+        if ( is_selected() )
+        {
+            fl_draw_box( prefs().selectbox(), X, Y, W, H, bg );
         }
-      }
-      if ( *(s+n) == treewc->column_char() ) {
-        s += n+1;               // skip field + delim
-        RX += col_widths[t++];  // keep track of fixed column widths for all except right column
-        continue;
-      } else {
-        // Last field? Return entire length of unclipped field
-        RX += fl_width(s) + m_LeftMargin;
-        s += n;
-      }
+        else
+        {
+            fl_color( bg );
+            fl_rectf( X, Y, W, H );
+        }
+        fl_font( labelfont(), labelsize() );
     }
-  }
-  if ( render ) fl_pop_clip();
-  return RX;                    // return right most edge of what we've rendered
+
+    // Render columns
+    if ( render_flag )
+    {
+        // Erco says "remember to fl_pop_clip after draw/calculate..."
+        fl_push_clip( X, Y, W, H );
+    }
+
+    {
+        // Draw each column or calculate width of row for return.
+        int t=0;
+        const char *s = label();
+        char delim_str[2] = { treewc->column_char(), 0 };  // strcspn() wants a char[]
+        while ( *s )
+        {
+            int n = strcspn( s , delim_str ); // find index to next delimiter char in 's' (or eos if none)
+
+            if ( n > 0 && render_flag )
+            {
+                int XX = ( t == 0 ) ? X : RX;
+
+                // col width clipping
+                int CW = m_ColWidths[t];
+
+                // if first column, clip to edge of next col
+                if ( t == 0 )
+                {
+                    CW = ( RX + m_ColWidths[0] ) - XX;
+                }
+
+                // If last column, clip to right edge of widget
+                if ( *( s + n ) == 0 )
+                {
+                    CW = ( x() + w() - XX );
+                }
+
+                // Draw the text
+                fl_color( fg );
+                fl_push_clip( XX, Y, CW, H ); // prevent text from running into next column
+                fl_draw( s, n, XX + m_LeftMargin, RY );
+                fl_pop_clip(); // clip off
+
+                // Draw vertical lines for all columns except first
+                if ( t > 0 && treewc->column_separators() )
+                {
+                    fl_color( FL_BLACK );
+                    fl_line( RX , Y , RX , Y + H );
+                }
+            }
+            if ( *( s + n ) == treewc->column_char() )
+            {
+                s += n+1;                // skip field + delim
+                RX += m_ColWidths[ t++ ]; // keep track of fixed column widths for all except right column
+                continue;
+            }
+            else
+            {
+                // Last field? Return entire length of unclipped field
+                RX += fl_width( s ) + m_LeftMargin;
+                s += n;
+            }
+        }
+    }
+
+    if ( render_flag )
+    {
+        fl_pop_clip();
+    }
+
+    // return right most edge of what we've rendered
+    return RX;
 }
 
 TreeWithColumns::TreeWithColumns( int X, int Y, int W, int H, const char *L ) : Fl_Tree( X, Y, W, H, L )
 {
-    colseps_flag     = true;
-    resizing_flag    = true;
-    col_char         = '\t';
-    col_widths       = 0;
-    first_col_minw   = 80;
-    drag_col         = -1;
-    last_cursor      = FL_CURSOR_DEFAULT;
+    m_ColSepFlag   = true;
+    m_ResizeFlag  = true;
+    m_ColChar       = '\t';
+    m_ColWidths     = 0;
+    m_FirstColMinW = 80;
+    m_DragCol       = -1;
+    m_LastCursor    = FL_CURSOR_DEFAULT;
+    m_MinColWidth  = 2;
+
     // We need the default tree icons on all platforms.
     //    For some reason someone made Fl_Tree have different icons on the Mac,
     //    which doesn't look good for this application, so we force the icons
     //    to be consistent with the '+' and '-' icons and dotted connection lines.
-    //
-    connectorstyle(FL_TREE_CONNECTOR_DOTTED);
-    openicon(new Fl_Pixmap(tree_open_xpm));
-    closeicon(new Fl_Pixmap(tree_close_xpm));
+    connectorstyle( FL_TREE_CONNECTOR_DOTTED );
+    openicon( new Fl_Pixmap( tree_open_xpm ) );
+    closeicon( new Fl_Pixmap( tree_close_xpm ) );
 }
 
 int TreeWithColumns::column_near_mouse()
 {
-    // Event not inside browser area? (eg. scrollbar) Early exit
-    if ( !Fl::event_inside(_tix,_tiy,_tiw,_tih) ) return(-1);
-    int mousex = Fl::event_x() + hposition();
-    int colx = x() + first_col_minw;
-    for ( int t=0; col_widths[t]; t++ ) {
-    colx += col_widths[t];
-    int diff = mousex - colx;
-    // Mouse near column? Return column #
-    if ( diff >= -4 && diff <= 4 ) return(t);
+    // exit if event not in browser area
+    if ( !Fl::event_inside( _tix, _tiy, _tiw, _tih ) )
+    {
+        return -1;
     }
-    return(-1);
-    // Change the mouse cursor
-    //     Does nothing if cursor already set to same value.
+
+    int mousex = Fl::event_x() + hposition();
+    int colx = x() + m_FirstColMinW;
+
+    int colgrab = 4;
+
+    // check which col divider, if any, are close to the mouse
+    for ( int t = 0; m_ColWidths[t]; t++ )
+    {
+        colx += m_ColWidths[t];
+        int diff = mousex - colx;
+        // return col# if mouse near cols
+        if ( diff >= -colgrab && diff <= colgrab )
+        {
+            return t;
+        }
+    }
+
+    return -1;
 }
 
 void TreeWithColumns::change_cursor( Fl_Cursor newcursor )
 {
-    if ( newcursor == last_cursor ) return;
-    window()->cursor(newcursor);
-    last_cursor = newcursor;
+    if ( newcursor == m_LastCursor )
+    {
+        return;
+    }
+
+    window()->cursor( newcursor );
+    m_LastCursor = newcursor;
 }
 
 TreeRowItem* TreeWithColumns::AddRow( const char *s, TreeRowItem *parent_item )
 {
-    TreeRowItem *item = new TreeRowItem(this, s);  // create new item
-    if ( !parent_item ) // wants root item as parent?
+    TreeRowItem *item = new TreeRowItem( this, s );
+
+    // if called without a parent item argument, check root assignment
+    if ( !parent_item )
     {
-      if ( !root() || ( strcmp( root()->label(), "ROOT" ) == 0 ) ) // default root item or no root
-      {
-          this->root(item);                          // make this item the new root
-          // Special colors for root item -- this is the "header"
-          item->labelfgcolor(0xffffff00);
-          item->labelbgcolor(0x8888ff00);
-          return item;
-      }
-      else
-      {
-          parent_item = (TreeRowItem*)root();        // use root as parent
-      }
+        // if TreeWithColumns has no root assigned or is on default root, designate this new item as root
+        if ( !root() || ( strcmp( root()->label(), "ROOT" ) == 0 ) )
+        {
+            this->root( item );
+
+            // Special colors for root item -- this is the "header"
+            item->labelfgcolor( 0xffffff00 );
+            item->labelbgcolor( 0x8888ff00 );
+
+            // return created TreeRowItem
+            return item;
+        }
+        else
+        {
+            // use root as parent
+            parent_item = ( TreeRowItem* )root();
+        }
     }
-    parent_item->add(prefs(), "", item);           // add item to hierarchy
-    return item;                                   // return the new item
+
+    // if parent item is in args, attach new item to it.
+    parent_item->add( prefs(), "", item );
+
+    // return created TreeRowItem
+    return item;
+}
+
+void TreeWithColumns::GetSelectedItems( vector < TreeRowItem* > *item_vec )
+{
+    Fl_Tree_Item_Array base_array;
+
+    get_selected_items( base_array );
+
+    for( int i = 0; i != base_array.total(); ++i )
+    {
+        TreeRowItem* treeRowItem = static_cast < TreeRowItem* >( base_array[ i ] );
+        if ( treeRowItem )
+        {
+            item_vec->push_back( treeRowItem );
+        }
+    }
 }
 
 int TreeWithColumns::handle( int e )
 {
-    if ( !resizing_flag ) return Fl_Tree::handle(e);    // resizing off? early exit
+    //exit handle method if not resizing
+    if ( !m_ResizeFlag )
+    {
+        return Fl_Tree::handle( e );
+    }
+
     // Handle column resizing
     int ret = 0;
-    switch ( e ) {
-    case FL_ENTER:
-        ret = 1;
-        break;
-    case FL_MOVE:
-        change_cursor( (column_near_mouse() >= 0) ? FL_CURSOR_WE : FL_CURSOR_DEFAULT);
-        ret = 1;
-        break;
-    case FL_PUSH: {
-        int whichcol = column_near_mouse();
-        if ( whichcol >= 0 ) {
-        // Clicked on resizer? Start dragging that column
-        drag_col = whichcol;
-        change_cursor(FL_CURSOR_DEFAULT);
-        return 1;   // eclipse event from Fl_Tree's handle()
+    switch ( e )
+    {
+        case FL_ENTER:
+            ret = 1;
+            break;
+        case FL_MOVE:
+            change_cursor( ( column_near_mouse() >= 0 ) ? FL_CURSOR_WE : FL_CURSOR_HAND );
+            ret = 1;
+            break;
+        case FL_PUSH:
+        {
+            int whichcol = column_near_mouse();
+            if ( whichcol >= 0 )
+            {
+                // drag a column if mouse close
+                m_DragCol = whichcol;
+                change_cursor( FL_CURSOR_DEFAULT );
+
+                // eclipse event from Fl_Tree's handle()
+                return 1;
+            }
+            break;
         }
-        break;
+        case FL_DRAG:
+            if ( m_DragCol != -1 )
+            {
+                // Sum up column widths to determine position
+                int mousex = Fl::event_x() + hposition();
+                int w_new = mousex - ( x() + first_column_minw() );
+
+                for ( int t = 0; m_ColWidths[t] && t < m_DragCol; t++ )
+                {
+                    w_new -= m_ColWidths[t];
+                }
+
+                // ensure w_new is at least minColWidth
+                w_new = ( m_MinColWidth > w_new ) ? m_MinColWidth: w_new;
+
+                // Apply new width, redraw interface
+                m_ColWidths[ m_DragCol ] = w_new;
+
+                recalc_tree();
+                redraw();
+
+                // eclipse event from Fl_Tree's handle()
+                return 1;
+            }
+            break;
+        case FL_LEAVE:
+        case FL_RELEASE:
+            // ensure normal cursor
+            change_cursor( FL_CURSOR_DEFAULT );
+
+            // release during drag mode?
+            if ( m_DragCol != -1 )
+            {
+                // disable drag mode and eclipse event from Fl_Tree's handle()
+                m_DragCol = -1;
+                return 1;
+            }
+
+            ret = 1;
+            break;
     }
-    case FL_DRAG:
-        if ( drag_col != -1 ) {
-        // Sum up column widths to determine position
-        int mousex = Fl::event_x() + hposition();
-        int newwidth = mousex - (x() + first_column_minw());
-        for ( int t=0; col_widths[t] && t<drag_col; t++ )
-            { newwidth -= col_widths[t]; }
-        // Apply new width, redraw interface
-        col_widths[drag_col] = newwidth;
-        if ( col_widths[drag_col] < 2 ) col_widths[drag_col] = 2;  // XXX: 2 should be a class member
-        recalc_tree();
-        redraw();
-        return 1;        // eclipse event from Fl_Tree's handle()
-        }
-        break;
-    case FL_LEAVE:
-    case FL_RELEASE:
-        change_cursor(FL_CURSOR_DEFAULT);          // ensure normal cursor
-        if ( drag_col != -1 && e == FL_RELEASE ) { // release during drag mode?
-        drag_col = -1;   // disable drag mode
-        return 1;        // eclipse event from base class; we handled it
-        }
-        drag_col = -1;
-        ret = 1;
-        break;
-    }
-    return(Fl_Tree::handle(e) ? 1 : ret);
+    return( Fl_Tree::handle(e) ? 1 : ret );
 }
--- src/gui_and_draw/TreeColumnWidget.h
+++ src/gui_and_draw/TreeColumnWidget.h
@@ -1,3 +1,13 @@
+//
+// This file is released under the terms of the NASA Open Source Agreement (NOSA)
+// version 1.3 as detailed in the LICENSE file which accompanies this software.
+//
+// TreeColumnWidget.h: a modified Fl_Tree widget based on the code provided at Erco's 
+//             FLTK Cheat Page: http://seriss.com/people/erco/fltk/#Fl_Resize_Browser
+// Tim Cuatt
+//
+//////////////////////////////////////////////////////////////////////
+
 #if !defined(TREECOL_WIDGET_INCLUDED_)
 #define TREECOL_WIDGET_INCLUDED_
 
@@ -5,6 +15,10 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Double_Window.H>
 #include <FL/Fl_Tree.H>
+
+#include <vector>
+
+using namespace std;
 
 //==== Tree With Columns ====//
 const char * const tree_open_xpm[] = {
@@ -47,67 +61,107 @@ const char * const tree_close_xpm[] = {
 class TreeRowItem : public Fl_Tree_Item
 {
 public:
-  TreeRowItem(Fl_Tree *tree, const char *text);
-  int draw_item_content(int render);
-  void SetLeftMargin( const int & left_margin )
-  {
-    m_LeftMargin = left_margin;
-  }
-  int GetLeftMargin()
-  {
-    return m_LeftMargin;
-  }
+    TreeRowItem( Fl_Tree *tree, const char *text );
+    int draw_item_content( int render );
+
+    void SetLeftMargin( const int & left_margin )
+    {
+        m_LeftMargin = left_margin;
+    }
+
+    int GetLeftMargin()
+    {
+       return m_LeftMargin;
+    }
+
 protected:
-  int m_LeftMargin;
+
+    int m_LeftMargin;
 };
 
 // Small convenience class to handle adding columns.
 //    TreeRowItem does most of the work.
-//
+
 class TreeWithColumns : public Fl_Tree
 {
-  protected:
-    int column_near_mouse();
-    void change_cursor(Fl_Cursor newcursor);
+    protected:
+        int column_near_mouse();
+        void change_cursor( Fl_Cursor newcursor );
 
-  public:
-    TreeWithColumns(int X,int Y,int W,int H,const char *L=0);
+        bool m_ColSepFlag;
+        bool m_ResizeFlag;
+        char m_ColChar;
+        int *m_ColWidths;
+        int m_FirstColMinW;
+        int m_DragCol;
+        int m_MinColWidth;
+        Fl_Cursor m_LastCursor;
 
-    bool colseps_flag;     // enable/disable column separator lines
-    bool resizing_flag;    // enable/disable interactive resizing
-    char col_char;         // column delimiter character
-    int *col_widths;       // array of column widths (supplied by application)
-    int first_col_minw;    // minimum width of first column
-    int drag_col;          // internal: column being FL_DRAG'ed
-    Fl_Cursor last_cursor; // internal: last mouse cursor value
+    public:
+       TreeWithColumns( int X, int Y, int W, int H, const char *L = 0 );
 
-    int  first_column_minw()          { return first_col_minw; }
-    void first_column_minw(int val)   { first_col_minw = val; }
-    // Enable/disable the vertical column lines
-    void column_separators(bool val)  { colseps_flag = val; }
-    bool column_separators() const    { return colseps_flag; }
-    // Enable/disable the vertical column lines
-    void resizing(bool val)           { resizing_flag = val; }
-    bool resizing() const             { return resizing_flag; }
-    // Change the column delimiter character
-    void column_char(char val)        { this->col_char = val; }
-    char column_char() const          { return col_char; }
-    // Set the column array.
-    //     Make sure the last entry is zero.
-    //     User allocated array must remain allocated for lifetime of class instance.
-    //     Must be large enough for all columns in data!
-    //
-    void column_widths(int *val)      { this->col_widths = val; }
-    int *column_widths() const        { return col_widths; }
-    TreeRowItem *AddRow(const char *s, TreeRowItem *parent_item = nullptr );
+        int  first_column_minw()
+        {
+            return m_FirstColMinW;
+        }
+        void first_column_minw( int val )
+        {
+            m_FirstColMinW = val;
+        }
 
-    // Manage column resizing
-    int handle( int e );
+        // Enable/disable the vertical column lines
+        void column_separators( bool val )
+        {
+            m_ColSepFlag = val;
+        }
+        bool column_separators() const
+        {
+            return m_ColSepFlag;
+        }
 
-    // Hide these base class methods from the API; we don't want app using them,
-    // as we expect all items in the tree to be TreeRowItems, not Fl_Tree_Items.
-  private:
-    using Fl_Tree::add;
+        void resizing( bool val )
+        {
+            m_ResizeFlag = val;
+        }
+        bool resizing() const
+        {
+            return m_ResizeFlag;
+        }
+
+        // Change the column delimiter character
+        void column_char( char val )
+        {
+            this->m_ColChar = val;
+        }
+        char column_char() const
+        {
+            return m_ColChar;
+        }
+
+        // Set the column array.
+        //     Make sure the last entry is zero.
+        //     User allocated array must remain allocated for lifetime of class instance.
+        //     Must be large enough for all columns in data!
+        void column_widths( int *val )
+        {
+            this->m_ColWidths = val;
+        }
+        int *column_widths() const
+        {
+            return m_ColWidths;
+        }
+
+        TreeRowItem *AddRow( const char *s, TreeRowItem *parent_item = nullptr );
+
+        void GetSelectedItems( vector < TreeRowItem* > *item_vec );
+
+        // Manage column resizing
+        int handle( int e );
+
+        // Hide these base class methods from the API; we don't want app using them,
+        // as we expect all items in the tree to be TreeRowItems, not Fl_Tree_Items.
+    private:
+        using Fl_Tree::add;
 };
 
 // Handle custom drawing of the item
